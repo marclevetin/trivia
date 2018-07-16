8ffb1a9e2c16285990e06c9433e7822e5a5ccf39
marclevetin <marc@derbyhat.org>
2018-07-15 21:41:42 -0400

    added questions

---
+++ assets/js/game.js
@@ -0,0 +1,62 @@
+const questions = [
+    {
+        id: 1,
+        question: "Who are Doc's patient's?",
+        answers: ['Vegetables', 'Animals', 'Minerals', 'Toys'],
+        correctAnswer: 'Toys'
+    },
+    {
+        id: 2,
+        question: "What is Chilly the Snowman always worried about?",
+        answers: ["Melting", "Eating", "Being left behind", ""],
+        correctAnswer: 'Melting'
+    },
+    {
+        id: 3,
+        question: "What does Lambie always give people?",
+        answers: ["Hugs", "Cheetos", "Wool", "High Fives"],
+        correctAnswer: 'Hugs'
+    },
+    {
+        id: 4,
+        question: "Which First Lady invited Doc to the White House?",
+        answers: ["Michelle Obama", "Melania Trump", "Hillary Clinton", "Laura Bush"],
+        correctAnswer: 'Michelle Obama'
+    },
+    {
+        id: 5,
+        question: "What tool does Doc use to bring her toys to life?",
+        answers: ["A Wrench", "A Stethoscope", "A Light Bulb", "A Hamster Wheel"],
+        correctAnswer: 'A Stethoscope'
+    },
+    {
+        id: 6,
+        question: "What is the first thing that Doc checks, according the song, Time For Your Checkup?",
+        answers: ["Ears", "Eyes", "Heartbeat", "Height"],
+        correctAnswer: 'Ears'
+    },
+    {
+        id: 7,
+        question: "Who brings over the 'Big Book of Boo Boos' to record diagnoses?",
+        answers: ["Lambie", "Stuffy", "Hallie", "Chilly"],
+        correctAnswer: 'Hallie'
+    },
+    {
+        id: 8,
+        question: "What type of animal is Stuffy?",
+        answers: ["Dragon", "Dinosaur", "Sheep", "Dog"],
+        correctAnswer: 'Dragon'
+    },
+    {
+        id: 9,
+        question: "Who are Doc's neighbors?",
+        answers: ["Emmie and Alma", "Ernie and Bert", "Ricardo and Richie", "Felix and Oscar"],
+        correctAnswer: 'Emmie and Alma'
+    },
+    {
+        id: 10,
+        question: "What is Doc's real name?",
+        answers: ["Dottie", "Sarah", "Debbie", "Michelle"],
+        correctAnswer: 'Dottie'
+    },
+]
